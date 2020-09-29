cda4b331251f0a3959a7c17594ce8f9cae3f9a0a
Samir Niraula <samniraula@gmail.com>
2020-09-29 07:33:06 +0545

    Added the api to query the database at /data

--- geoproject/frontend/views.py
+++ geoproject/frontend/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from django.core.files.storage import FileSystemStorage
+from rest_framework.parsers import JSONParser
+from .models import data
+from .serializers import dataSerializer
 # Create your views here.
 
 
 def index(request):
     if request.method == 'POST':
         uploaded_file = request.FILES['document']
-        fs = FileSystemStorage(location="./Excelfiles/")
+        fs = FileSystemStorage(location="../media/Excelfiles/")
         fs.save(uploaded_file.name, uploaded_file)
 
     return render(request, 'frontend/index.html')
+
+
+def data_list(request):
+    if request.method == 'GET':
+        values = data.objects.all()
+        serializer = dataSerializer(values, many=True)
+        return JsonResponse(serializer.data, safe=False)

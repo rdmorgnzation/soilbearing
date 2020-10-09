d518c78e5643c7c6524f651092f2b9fe5ef0a1ee
Samir Niraula <samniraula@gmail.com>
2020-10-09 14:01:12 +0545

    added a basic canvas

--- web/js/src/components/layout/finfo.js
+++ web/js/src/components/layout/finfo.js
@@ -1,18 +1,45 @@
-import React, { Component } from "react";
+import React, { Component, useRef, useEffect } from "react";
 
 class finfo extends Component {
+
     render() {
+        const Canvas = props => {
+
+            const canvasRef = useRef(null)
+
+
+
+            const draw = ctx => {
+                ctx.fillStyle = '#00ff00';
+                ctx.fillRect(0, 200, 600, 600);
+                ctx.fillStyle = '#ff00ff';
+                ctx.beginPath()
+                ctx.fillRect(200, 0, 200, 250);
+                ctx.fillStyle = '#ff00ff';
+                ctx.fillRect(0, 250, 600, 100);
+                ctx.beginPath();
+                ctx.fillStyle = '#ff000f';
+                ctx.fillRect(0, 350, 600, 200);
+                ctx.beginPath();
+                ctx.fillStyle = '#f3370f';
+                ctx.fillRect(0, 550, 600, 50);
+            }
+
+            useEffect(() => {
+
+                const canvas = canvasRef.current;
+                const context = canvas.getContext('2d')
+
+                draw(context)
+            }, [draw])
+
+            return <canvas width="600" height="600" ref={canvasRef} {...props} />
+        }
+
         return (
             <div>
-                <h2>HELLO</h2>
-                <p>larum*60ada d ajkfnas ac  casalsdka;lsdma;ldm;llfaklndada
-                dadlamsdlasda
-                dalksdmas'dalksdmasdafkalndmal;ma
-                daklfnasl;dalksdmasdafkalndmaldadklanfalsk
-                </p>
-
-                <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
-            </div>
+                <Canvas />
+            </div >
         );
     }
 }

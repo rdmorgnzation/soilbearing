a89ab2c75b2675afe31391e3cacd57a0ad8329a1
Rupak Bajgain <nnew234567@gmail.com>
2021-02-18 09:31:26 +0545

    Edit start page

--- js/src/components/main_page.jsx
+++ js/src/components/main_page.jsx
@@ -6,7 +6,11 @@ class results extends React.Component {
   render() {
     return (
       <div>
-        <h2>Main content</h2>
+        <h2>How to use</h2>
+        - Upload excel file if available(optional)<br/>
+        - Edit datas as necessary<br/>
+        - Calculate result<br/>
+        - Map page is available for Bearing capacity map<br/>
         <h2>Attributes</h2>
         <Attributes />
       </div>
